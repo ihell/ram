3cc2b8dba3d2cd2d8d25e6364c0843bdbf775d57
ihell <ihell269@gmail.com>
2025-06-01 21:37:04 +0700

    Add react-icons and enhance Header component with social icons and a hire button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
 import { motion } from "framer-motion";
+import { a } from "framer-motion/client"
+import { FiGithub, FiInstagram, FiLinkedin,} from "react-icons/fi";
 
 const Header = () => {
     return (
@@ -37,7 +39,7 @@ const Header = () => {
             </motion.div>
             
             {/* Desktop Navigation */}
-            <div className="lg:flex hidden space-x-8">
+            <nav className="lg:flex hidden space-x-8">
                 {["Home", "About", "Projects", "Contact"].map((item, index) => (
 
                     <motion.a 
@@ -50,9 +52,8 @@ const Header = () => {
                         damping: 20,
                         delay: 0.7 + index * 0.2,
                     }}
-                    className="relative text-gray-800 dark:tex-gray-200
-                    hover:violet-600 dark:hover:text-violet-400 font-medium
-                    transition-colors duration-300 group"
+                    className="relative text-xl font-bold bg-gradient-to-r from-gray-300 to-gray-100 bg-clip-text text-transparent
+                    hover:text-violet-600 dark:hover:text-violet-400 transition-colors duration-300 group"
                     href="#">
                         {item}
                         <span className="absolute bottom-0 left-0 w-0 h-0.5
@@ -61,7 +62,56 @@ const Header = () => {
                     </motion.a> 
 
                 ))}
+            </nav>
+
+            {/* sosial icon */}
+            <div className="md:flex hidden items-center space-x-4">
+
+                <motion.a
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ delay: 1.3, duration: 0.8 }}
+                className="text-gray-700 dark:text-gray-300 hover:text-violet-600
+                dark:hover:text-violet-400 transition-colors duration-300" href="#">
+                    <FiGithub className="w-5 h-5" />
+                </motion.a>
+
+                <motion.a
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ delay: 1.3, duration: 0.8 }}
+                className="text-gray-700 dark:text-gray-300 hover:text-violet-600
+                dark:hover:text-violet-400 transition-colors duration-300" href="#">
+                    <FiInstagram className="w-5 h-5" />
+                </motion.a>
+
+                <motion.a
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ delay: 1.3, duration: 0.8 }}
+                className="text-gray-700 dark:text-gray-300 hover:text-violet-600
+                dark:hover:text-violet-400 transition-colors duration-300" href="#">
+                    <FiLinkedin className="w-5 h-5" />
+                </motion.a>
             </div>
+
+            {/* hire me button */}
+            <motion.button 
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ 
+                delay: 1.5,
+                duration: 0.8,
+                type: "spring",
+                stiffness: 100, 
+                damping: 15,
+              }}
+            className="ml-4 px-4 py-2 rounded-xl bg-gradient-to-r
+            from-gray-400 to-gray-100 text-violet-700 font-bold
+            hover:from-violet-700 hover:to-purple-700 hover:text-white
+            transition-all duration-500">
+                Hire Me
+            </motion.button>
 
         </div>
 
