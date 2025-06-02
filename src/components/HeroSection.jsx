bd37b1d82d42735a2ce1365ad06a932dfb199a41
ihell <ihell269@gmail.com>
2025-06-02 20:44:35 +0700

    Add Spline integration and enhance HeroSection with descriptive text

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,5 @@
 import { motion } from 'framer-motion';
+import Spline from '@splinetool/react-spline';
 
 const HeroSection = () => {
   return (
@@ -8,7 +9,7 @@ const HeroSection = () => {
    lg:px-24 px-10 relative overflow-hidden">
 
     {/* left section */}
-    <div>
+    <div className='z-40 xl:mb-0 mb-[20%]'>
         <motion.h1
             initial={{ opacity: 0, y: 80 }}
             animate={{ opacity: 1, y: 0 }}
@@ -24,7 +25,39 @@ const HeroSection = () => {
             Building Fast <br /> Reliable
             Results
         </motion.h1>
+
+        <motion.p
+            initial={{ opacity: 0, y: 80 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+                type: 'spring',
+                stiffness: 40,
+                damping: 25,
+                delay: 1.8,
+                duration: 1.5
+            }}
+            className='text-xl md:text-1xl
+            lg:text-2xl text-purple-200 max-w-2xl'>
+                 Lorem ipsum dolor sit amet,
+                 consectetur adipiscing elit.
+                 Nunc eu consectetur nisi.
+                 Fusce id molestie lacus.
+                 Nam interdum erat in risus blandit,
+                 eu pretium mauris porttitor.
+                 Aenean non ultrices nunc.
+                 Mauris viverra pulvinar est non pharetra.
+                 Pellentesque id vulputate odio.
+                 Nunc condimentum felis sed ex finibus laoreet.
+                 Duis maximus, enim in molestie maximus,
+                 neque erat sagittis purus, ac elementum ante urna nec tortor.
+                 Morbi eget nisi quis ante venenatis molestie ut eget nulla.
+                 Aenean eget nisl volutpat, feugiat metus at, vulputate massa. 
+        </motion.p>
     </div>
+
+    {/* right section */}
+    <Spline className='absolute xl:right-
+    [-28%] right-0 top-[20%] lg:top-0' scene="https://prod.spline.design/Nj7pyJ0fVqbDsq3S/scene.splinecode" />
 
    </section>
   )
